64227c74feaaf64b3abde95c0a0bb9692c706249
AgelxNash <iam@agel-nash.ru>
2013-05-22 11:51:28 +0600

    History

--- resourse.php
+++ resourse.php
@@ -22,7 +22,7 @@ $resourse->document()->set('titl','Пропаганда')->set('pagetitle',$i)->
 
 #-------------------------------------------------------
 #Edit resourse #13 
-$resourse->newDoc(13)->set('pagetitle','new pagetitle')->save(null,false);
+$resourse->document(13)->set('pagetitle','new pagetitle')->save(null,false);
 
 #-------------------------------------------------------
 $resourse->delete(8);
